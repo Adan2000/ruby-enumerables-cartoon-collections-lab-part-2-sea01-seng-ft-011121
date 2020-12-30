47088604213db723e3ac279fa7fde7a1187fda73
Adan Escamilla <adanescamilla580@gmail.com>
2020-12-30 06:54:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   
   
-  planeteer_calls.any? do |element|
+  planeteer_calls.all? do |element|
       element.length > 4
 
 end
